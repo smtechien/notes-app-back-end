86973114405f5068f2d5c6ee4565a925b7b8e5ff
smtekien <sulthonrahmatullah@gmail.com>
2022-03-19 13:46:54 +0800

    data validation  implemented

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes/index');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -13,7 +16,13 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada alamat ${server.info.uri}`);
